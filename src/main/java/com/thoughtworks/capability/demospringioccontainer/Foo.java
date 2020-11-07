1c5b3e5341ba440b3737ae372b69c55d3f6f5a29
Xiao Yu <xiao.susan.yu@gmail.com>
2020-11-07 19:32:40 +0800

    feature: use PostConstruct

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,15 +1,21 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Foo {
 
+    @Autowired
     private Bar bar;
 
-    public Foo(Bar bar) {
-        this.bar = bar;
+    @PostConstruct
+    public void init() {
+        bar.setFoo(this);
     }
+
 
     public void hi() {
         System.out.println("Hi, " + bar.name());
